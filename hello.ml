655a3b91c2f58d0a847bdb90de8ff6282109cba2
Daniel Kim <bjkim0526@gmail.com>
2016-07-19 10:24:27 -0700

    added stuff

--- hello.ml
+++ hello.ml
@@ -1,4 +1,23 @@
-print_string "Hello World! \n"
+open Core.Std
 
-(*exit 0;; (* Comments *) *)
+let build_counts () =
+  In_channel.fold_lines stdin ~init:[] ~f:(fun counts line ->
+    let count =
+      match List.Assoc.find counts line with
+      | None -> 0
+      | Some x -> x
+    in
+    List.Assoc.add counts line (count + 1)
+  )
 
+let () =
+  build_counts ()
+  |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
+  |> (fun l -> List.take l 10)
+  |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+
+
+  (* OCAML does not have a unique main function. When an OCAML program is evaluated, all the statemenets
+  are in the implementation files are evaluated in the order in which they were linked together.)
+  
+ *)  
